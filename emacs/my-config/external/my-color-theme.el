8b20ff058188ed886376cd26a503b254a2ae49ee
Zhiwei Xiao <zwxiao@gmail.com>
2012-04-09 10:22:06 +0800

    set font only in x window

--- emacs/my-config/external/my-color-theme.el
+++ emacs/my-config/external/my-color-theme.el
@@ -10,17 +10,20 @@
   '(progn
      (color-theme-initialize)
      (if (< (display-color-cells) 20)
-	 (color-theme-tty-dark)
+	   (color-theme-tty-dark)
        (color-theme-solarized-light))
 ;      (color-theme-billw))
 ))
 
 ;; font
-(set-default-font "Mono-14")
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font)
-                        charset
-                        "Hiragino Sans GB-18"))
+; set only under x-window
+(when (display-graphic-p)
+  (set-default-font "Mono-14")
+  ; Chinese font
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+		      charset
+		      "Hiragino Sans GB-18")))
 
 (setq-default line-spacing 3)
 
