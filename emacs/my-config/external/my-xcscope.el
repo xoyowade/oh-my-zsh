7a463703fd3057dfa18c7ae9887bd280b6684943
Zhiwei Xiao <zwxiao@gmail.com>
2014-02-27 16:22:19 +0800

    disable cscope auto rebuild database

--- emacs/my-config/external/my-xcscope.el
+++ emacs/my-config/external/my-xcscope.el
@@ -6,5 +6,5 @@
 (require 'xcscope)
 (setq cscope-use-face nil)
 ;; for large project, disable auto update 
-;(setq cscope-do-not-update-database t)
+(setq cscope-do-not-update-database t)
 ;;; my-xcscope.el ends here
